31f222769471877fe9472a31b28d8bb0875729cb
Max Effron <maxfron@gmail.com>
2019-08-25 17:04:21 -0700

    Initial Commit

---
+++ Inspire Navigation Demo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Inspire Navigation Demo
+//
+//  Created by Max Effron on 8/25/19.
+//  Copyright © 2019 Max Effron. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Inspire Navigation Demo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Inspire Navigation Demo
+//
+//  Created by Max Effron on 8/25/19.
+//  Copyright © 2019 Max Effron. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
